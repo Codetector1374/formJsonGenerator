c31fb09de2847299c0bb9cb61318c65b1c669f5e
Codetector <codetector@codetector.cn>
2017-02-25 12:30:11 -0500

    Release 1.0

--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="app">
-        <transition :name="transitionName" mode="out-in">
-            <router-view class="child-transition"></router-view>
+    <div id='app'>
+        <transition :name='transitionName' mode='out-in'>
+            <router-view class='child-transition'></router-view>
         </transition>
-        <!--<footer class="footer">-->
-            <!--<div class="container">-->
-                <!--<p class="text-muted">© 2017 木轮子打气筒</p>-->
-            <!--</div>-->
+        <!--<footer class='footer'>-->
+        <!--<div class='container'>-->
+        <!--<p class='text-muted'>© 2017 木轮子打气筒</p>-->
+        <!--</div>-->
         <!--</footer>-->
     </div>
 </template>
@@ -20,8 +20,38 @@
                 transitionName: 'slide-left'
             }
         },
-        methods: {
-        }
+        create () {
+            const {menu} = self.$electron.remote
+            const app = self.$electron.remote
+            const template = [{
+                label: 'Application',
+                submenu: [
+                    {label: 'About Application', selector: 'orderFrontStandardAboutPanel:'},
+                    {type: 'separator'},
+                    {
+                        label: 'Quit',
+                        accelerator: 'Command+Q',
+                        click: function () {
+                            app.quit()
+                        }
+                    }
+                ]
+            }, {
+                label: 'Edit',
+                submenu: [
+                    {label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
+                    {label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
+                    {type: 'separator'},
+                    {label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
+                    {label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
+                    {label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'},
+                    {label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:'}
+                ]
+            }
+            ]
+            menu.setApplicationMenu(menu.buildFromTemplate(template))
+        },
+        methods: {}
     }
 </script>
 
@@ -34,6 +64,7 @@
         overflow: hidden;
         /*margin-top: 60px;*/
     }
+
     .slide-left-enter, .slide-right-leave-active {
         opacity: 0;
         -webkit-transform: translate(100px, 0);
@@ -56,6 +87,7 @@
         /* Margin bottom by footer height */
         /*margin-bottom: 60px;*/
     }
+
     .footer {
         position: absolute;
         bottom: 0;
@@ -67,7 +99,7 @@
     }
 
     /*.container .text-muted {*/
-        /*margin: 20px 0;*/
+    /*margin: 20px 0;*/
     /*}*/
 
 </style>
